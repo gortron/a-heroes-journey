2f820cae50fbfb021c461bc6e1047efa8cfb24ea
gortron <gordon.lanza@gmail.com>
2019-10-21 17:01:16 +0100

    moved hero initialization to hero.rb (SOC)

--- lib/hero.rb
+++ lib/hero.rb
@@ -1,13 +1,14 @@
 class Hero < ActiveRecord::Base
   has_many :journeys
   has_many :challenges, through: :journeys
+  after_initialize :init
 
   # Why does the intialize method below not work?
-  # def initialize(proc)
-  #   @name = proc[:name]
-  #   @story = proc[:story]
-  #   @experience = proc[:experience]
-  #   @health = proc[:health]
-  #   @power = proc[:power]
-  # end
+  def init
+    self.story ||= "A battle hardened veteran."
+    self.experience ||= 1
+    self.health ||= 100
+    self.power ||= 20
+    self.save
+  end
 end
